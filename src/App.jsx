35adb88b6d1e501fca4d60ef12d65a3fedd44281
Carl Limsico <limsico@Carls-Laptop.local>
2026-05-21 17:18:06 -0700

    fixed logo error

--- src/App.jsx
+++ src/App.jsx
@@ -31,20 +31,6 @@ function App() {
           </svg>
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
         </div>
         <div id="social">
           <svg className="icon" role="presentation" aria-hidden="true">
